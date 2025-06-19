12957e617c1061925168d77e2b1328d9e8ad7c46
isroil242332 <isroil-islam@mail.ru>
2025-06-19 16:51:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    clicks = db.Column(db.Integer, default=1)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Имя пользователя уже занято')
+            return redirect(url_for('register'))
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('game'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('game'))
+        else:
+            flash('Неверный логин или пароль')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def game():
+    if request.method == 'POST':
+        current_user.clicks += 1
+        db.session.commit()
+    return render_template('game.html', clicks=current_user.clicks, username=current_user.username)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
